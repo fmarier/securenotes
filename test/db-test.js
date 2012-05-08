527d8270528f4db386b9cfc976ea851a25f6d393
Francois Marier <francois@fmarier.org>
2012-05-08 14:55:46 +1200

    Add notes to the db tests

--- test/db-test.js
+++ test/db-test.js
@@ -7,6 +7,7 @@
 const
 assert = require('assert'),
 users = require('../lib/users.js'),
+notes = require('../lib/notes.js'),
 vows = require('vows');
 
 var suite = vows.describe("basic tests");
@@ -14,6 +15,7 @@ suite.options.error = false;
 
 const TEST_EMAIL = 'person@example.com';
 
+// TODO: test error conditions
 suite.addBatch(
     {
         "account creation": {
@@ -52,6 +54,180 @@ suite.addBatch(
                             "it is gone": function (r){
                                 assert.ok(!r);
                             }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+// TODO: test error conditions
+suite.addBatch(
+    {
+        "a new user": {
+            topic: function () {
+                var that = this;
+                users.create_user(
+                    TEST_EMAIL, function (email) {
+                        users.get_account(
+                            email, function (user_id) {
+                                notes.get_notes(user_id, that.callback);
+                            });
+                    });
+            },
+            "has no notes": function (r) {
+                assert.isArray(r);
+                assert.strictEqual(r.length, 0);
+            },
+            "can create new notes": {
+                topic: function () {
+                    var that = this;
+                    users.get_account(
+                        TEST_EMAIL, function (user_id) {
+                            notes.create_note(
+                                "Note 1", "Content 1", user_id, function () {
+                                    notes.get_notes(
+                                        user_id, function (r) {
+                                            var note_id = r[0].id;
+                                            notes.view_note(
+                                                note_id, user_id, function (note) {
+                                                    that.callback(r, note);
+                                                });
+                                        });
+                                });
+                        });
+                },
+                "that exist": function (r) {
+                    assert.isArray(r);
+                    assert.strictEqual(r.length, 1);
+                },
+                "that look reasonable": function (r) {
+                    var element = r[0];
+                    assert.isObject(element);
+                    assert.isNumber(element.id);
+                    assert.ok(element.id > 0);
+                    assert.strictEqual(element.deleted, 0);
+                    assert.strictEqual(element.name, 'Note 1');
+                },
+                "that have the expected content": function (r, note) {
+                    assert.isObject(note);
+                    assert.strictEqual(note.content, 'Content 1');
+                    assert.strictEqual(note.name, r[0].name);
+                },
+                "before deleting the user account": {
+                    topic: function () {
+                        users.delete_user(TEST_EMAIL, this.callback);
+                    },
+                    "successfully": function () {
+                        assert.ok(true); // TODO: check for lack of errors
+                    }
+                }
+            }
+        }
+    });
+
+var data = [
+    {name: 'Note 1', content: 'Content 1'},
+    {name: 'Note 2', content: 'Content 2'},
+    {name: 'Note 3', content: 'Content 3'},
+    {name: 'Note 4', content: 'Content 4'}
+];
+
+// TODO: test error conditions
+suite.addBatch(
+    {
+        "creating multiple notes": {
+            topic: function () {
+                var that = this;
+                users.create_user(
+                    TEST_EMAIL, function (email) {
+                        users.get_account(
+                            email, function (user_id) {
+                                var count = 0;
+                                data.forEach(
+                                    function (v) {
+                                        notes.create_note(
+                                            v.name, v.content, user_id, function () {
+                                                count += 1;
+                                                if (count === data.length) {
+                                                    notes.get_notes(user_id, that.callback);
+                                                }
+                                            });
+                                    });
+                            });
+                    });
+            },
+            "returns the expected array": function (r) {
+                assert.isArray(r);
+                assert.strictEqual(r.length, data.length);
+            },
+            "gives non-deleted notes": function (r) {
+                r.forEach(function (note) {
+                              assert.strictEqual(note.deleted, 0);
+                          });
+            },
+            "that can be deleted": {
+                topic: function () {
+                    var that = this;
+                    users.get_account(
+                        TEST_EMAIL, function (user_id) {
+                            notes.get_notes(
+                                user_id, function (r) {
+                                    var count = 0;
+                                    r.forEach(
+                                        function (note) {
+                                            notes.delete_note(
+                                                note.id, user_id, function () {
+                                                    count += 1;
+                                                    if (count === data.length) {
+                                                        notes.get_notes(user_id, that.callback);
+                                                    }
+                                                });
+                                        });
+                                });
+                        });
+                },
+                "successfully": function (r) {
+                    assert.isArray(r);
+                    assert.strictEqual(r.length, data.length);
+                    r.forEach(function (note) {
+                                  assert.strictEqual(note.deleted, 1);
+                              });
+                },
+                "and undeleted": {
+                    topic: function () {
+                        var that = this;
+                        users.get_account(
+                            TEST_EMAIL, function (user_id) {
+                                notes.get_notes(
+                                    user_id, function (r) {
+                                        var count = 0;
+                                        r.forEach(
+                                            function (note) {
+                                                notes.undelete_note(
+                                                    note.id, user_id, function () {
+                                                        count += 1;
+                                                        if (count === data.length) {
+                                                            notes.get_notes(user_id, that.callback);
+                                                        }
+                                                    });
+                                            });
+                                    });
+                            });
+                    },
+                    "successfully": function (r) {
+                        assert.isArray(r);
+                        assert.strictEqual(r.length, data.length);
+                        r.forEach(function (note) {
+                                      assert.strictEqual(note.deleted, 0);
+                                  });
+                    },
+                    "before deleting the user account": {
+                        topic: function () {
+                            users.delete_user(TEST_EMAIL, this.callback);
+                        },
+                        "successfully": function () {
+                            assert.ok(true); // TODO: check for lack of errors
                         }
                     }
                 }
