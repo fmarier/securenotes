e21b2458e9fc334f2540da6380586bbe2f6cabd9
Francois Marier <francois@mozilla.com>
2012-07-03 09:49:45 +1200

    Password not needed for MySQL

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "CREATE DATABASE securenotes CHARACTER SET utf8;" | mysql -uroot -p
-echo "GRANT ALL ON securenotes.* TO nodeuser IDENTIFIED BY 'nodeuser';" | mysql -uroot -p
+echo "CREATE DATABASE securenotes CHARACTER SET utf8;" | mysql -uroot
+echo "GRANT ALL ON securenotes.* TO nodeuser IDENTIFIED BY 'nodeuser';" | mysql -uroot
 mysql -unodeuser -pnodeuser securenotes < schema.sql
